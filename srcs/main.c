aaae58cac617db2eed7ae1b7032eefbd4c8e0297
Adrien Chardon <adrienchardon@mailoo.org>
2016-09-23 16:08:39 +0200

    Add some comments + refacto

--- srcs/main.c
+++ srcs/main.c
@@ -13,6 +13,10 @@ s_game *pn_init(void)
 {
     s_game *game = NULL;
 
+    /*
+        Malloc of main struct
+    */
+
     game = malloc(sizeof(s_game));
     if (game == NULL)
     {
@@ -21,11 +25,19 @@ s_game *pn_init(void)
     }
     memset(game, 0, sizeof(s_game));
 
+    /*
+        SDL
+    */
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         fprintf(stderr, "Error SDL_Init(): %s\n", SDL_GetError());
         exit(1);
     }
+
+    /*
+        Window and screen surface
+    */
 
     game->window = SDL_CreateWindow(
         "Open Source Ninja",
@@ -48,66 +60,9 @@ s_game *pn_init(void)
         exit(1);
     }
 
-    fruit_model_append(game, fruit_model_new(1, "Firefox"));
-    fruit_model_append(game, fruit_model_new(1, "Blender"));
-    fruit_model_append(game, fruit_model_new(1, "Gimp"));
-    fruit_model_append(game, fruit_model_new(1, "vlc"));
-    fruit_model_append(game, fruit_model_new(0, "Windows"));
-    fruit_model_append(game, fruit_model_new(0, "OS X"));
-
-    game->lives = 3;
-
-    return game;
-}
-
-
-void pn_update_events(int *quit)
-{
-    SDL_Event e;
-
-    while (SDL_PollEvent(&e) != 0)
-    {
-        if (e.type == SDL_QUIT)
-        {
-            *quit = 1;
-        }
-        else if (e.type == SDL_KEYDOWN)
-        {
-            switch (e.key.keysym.sym)
-            {
-                case SDLK_q:
-                case SDLK_ESCAPE:
-                    *quit = 1;
-                    break;
-
-                // case SDLK_UP:
-                //     gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
-                //     break;
-                // case SDLK_DOWN:
-                //     gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
-                //     break;
-
-                default:
-                    // gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
-                    break;
-            }
-        }
-        // else if (e.type == SDL_MOUSEMOTION)
-        // {
-        //     printf("%d %d\n", e.motion.x, e.motion.y);
-        // }
-    }
-
-}
-
-
-int main(int argc, char *argv[])
-{
-    s_game *game;
-    int quit = 0;
-
-    srand(42);
-    game = pn_init();
+    /*
+        SDL_ttf
+    */
 
     if (TTF_Init() == -1)
         printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
@@ -120,6 +75,111 @@ int main(int argc, char *argv[])
     game->font_text = TTF_OpenFont("res/Courier_New.ttf", 20);
     if (game->font_text == NULL)
         printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+
+    /*
+        Fruits
+    */
+
+    fruit_model_append(game, fruit_model_new(1, "Firefox"));
+    fruit_model_append(game, fruit_model_new(1, "Blender"));
+    fruit_model_append(game, fruit_model_new(1, "Gimp"));
+    fruit_model_append(game, fruit_model_new(1, "vlc"));
+    fruit_model_append(game, fruit_model_new(0, "Windows"));
+    fruit_model_append(game, fruit_model_new(0, "OS X"));
+
+    /*
+        Miscs
+    */
+
+    game->lives = 3;
+
+    return game;
+}
+
+void pn_free(s_game *game)
+{
+    int i = 0;
+
+    for (i = 0; i < FRUITS_MODEL_COUNT; ++i)
+    {
+        if (game->models[i] != NULL)
+        {
+            SDL_FreeSurface(game->models[i]->img);
+            free(game->models[i]);
+        }
+    }
+
+    for (i = 0; i < FRUITS_COUNT; ++i)
+    {
+        if (game->fruits[i] != NULL)
+            free(game->fruits[i]);
+    }
+
+    TTF_CloseFont(game->font_title);
+    TTF_CloseFont(game->font_text);
+    SDL_FreeSurface(game->screen);
+    SDL_FreeSurface(game->background);
+    SDL_DestroyWindow(game->window);
+
+    free(game);
+
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+}
+
+
+void pn_update_events(int *quit)
+{
+    SDL_Event e;
+
+    while (SDL_PollEvent(&e) != 0)
+    {
+        switch (e.type)
+        {
+            case SDL_QUIT:
+                *quit = 1;
+                break;
+
+            case SDL_KEYDOWN:
+                switch (e.key.keysym.sym)
+                {
+                    case SDLK_q:
+                    case SDLK_ESCAPE:
+                        *quit = 1;
+                        break;
+
+                    // case SDLK_UP:
+                    //     gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
+                    //     break;
+                    // case SDLK_DOWN:
+                    //     gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
+                    //     break;
+
+                    default:
+                        break;
+                }
+                break;
+
+            // case SDL_MOUSEMOTION:
+            //     printf("%d %d\n", e.motion.x, e.motion.y);
+            //     break;
+
+            default:
+                break;
+        }
+    }
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    s_game *game;
+    int quit = 0;
+
+    srand(42);
+    game = pn_init();
 
     while (!quit)
     {
@@ -144,14 +204,7 @@ int main(int argc, char *argv[])
         SDL_Delay(1000/FPS);
     }
 
-    TTF_CloseFont(game->font_title);
-    TTF_CloseFont(game->font_text);
-    TTF_Quit();
-    IMG_Quit();
-    SDL_FreeSurface(game->screen);
-    SDL_FreeSurface(game->background);
-    SDL_DestroyWindow(game->window);
-    SDL_Quit();
+    pn_free(game);
 
     return 0;
 }
